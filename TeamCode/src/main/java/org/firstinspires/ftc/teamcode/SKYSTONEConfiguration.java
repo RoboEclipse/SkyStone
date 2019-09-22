8d1d174ef7708f8c12992d28dd0bb1c4ceabc601
erichu100 <erichu100@outlook.com>
2019-09-22 14:20:21 -0700

    Copied over programs from old repository

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SKYSTONEConfiguration.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+class SKYSTONEConfiguration {
+    final String backLeftMotor = "backLeft";
+    final String frontLeftMotor = "frontLeft";
+    final String backRightMotor = "backRight";
+    final String frontRightMotor = "frontRight";
+    final String leftClawServo = "leftClawServo";
+    final String collectorServo = "collectorServo";
+
+}
