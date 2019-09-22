d5cdd68673bef3213ae95e5c31b72cc3084c8d16
ethan <ethanh8093@gmail.com>
2019-09-22 16:07:43 -0700

    Changed Constant

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SKYSTONEConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SKYSTONEConstants.java
@@ -8,7 +8,7 @@ import com.acmerobotics.dashboard.config.Config;
 //TODO: Re-add dashboard
 public class SKYSTONEConstants {
     //TicksPerRotation is 42.78 on testbot
-    public static double TICKS_PER_ROTATION = 103.6;
+    public static double TICKS_PER_ROTATION = 103.6/3.7;
     public static double GEAR_RATIO = 20;
     public static double TICKS_PER_WHEEL_ROTATION = TICKS_PER_ROTATION*GEAR_RATIO;
     public static double TICKS_PER_INCH = TICKS_PER_WHEEL_ROTATION/(4* Math.PI);
