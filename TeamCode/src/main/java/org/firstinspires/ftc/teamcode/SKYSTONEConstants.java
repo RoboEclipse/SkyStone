ff63543b85d6d5a73d789bc06ae1aaa0e1e1618d
ericzhou16 <ericsuper@outlook.com>
2019-10-31 20:48:57 -0700

    Decreased Bridge cross distance Decreased shift distance Decreased stoneDiff Decreased tighten Increased autoLoosen Moved the moving of claw slide earlier Slowed down encoder turn a little Added y for the log

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SKYSTONEConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SKYSTONEConstants.java
@@ -16,19 +16,19 @@ public class SKYSTONEConstants {
     public static double cSkybridgeClear = 20;
     public static double dSkyStoneAlign = -10;
     //Skystone Constants
-    public static double _pickUpDistance = 29;
+    public static double _pickUpDistance = 23;
     public static double _depotDistance = -24;
     public static double _aSkyStoneDistance = 18;
-    public static double _bBridgeCrossDistance = 55;
+    public static double _bBridgeCrossDistance = 42;
     public static double _cBridgeReturnDistance = -40;
     //Shared Constants
-    public static double shiftDistance = 8;
+    public static double shiftDistance = 7.5;
     public static double extraShift = 0;
     public static int raiseTicks = 600;
     public static int lowerTicks = -300;
     public static int extendSlide = -850;
     public static int safeSlide = -450; //Value when the rotator claw can turn without crashing into the sides
-    public static int stoneDiff = 5;
+    public static int stoneDiff = 3;
     //Skystone Tele-op Stacking Claw Rotation Servo Constants
     public static double zero = 0.495; //Angled straight
     public static double right90 = 0.385; //90 degrees right from straight
@@ -36,9 +36,9 @@ public class SKYSTONEConstants {
     public static double straight = 0.495; //Straight
     public static double oppositeSide = 0.72; //Make it straight but on the other side or back side
     //Skystone Tele-op Stacking Grabber Claw
-    public static double tighten = 0.2;
+    public static double tighten = 0.15;
     public static double loosen = 0.50;
-    public static double autoLoosen = 0.6;
+    public static double autoLoosen = 0.65;
     //Skystone Left Foundation Claw
     public static double lDown = 0.6; //At most .65
     public static double lUp = 0.25;
