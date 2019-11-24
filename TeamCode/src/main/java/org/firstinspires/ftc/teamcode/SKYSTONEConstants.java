06a3b40c089a31ce97a7e4fb9e36e25d44ede962
erichu100 <erichu100@outlook.com>
2019-11-24 15:33:18 -0800

    DoubleAutonomousRed fully tuned -Changed distanceEncoderDrive to not use encoders -Increased p value of distanceEncoderDrive to 20 -Put power at minimum of 0.2 -Added distances to sensorTest -Tuned constants -Cleaned up if statements -Moved left strafe to before driving forward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SKYSTONEConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SKYSTONEConstants.java
@@ -32,7 +32,7 @@ public class SKYSTONEConstants {
     public static double doubleSkyStoneDistance1 = 5;
     public static double doubleAdjustDistance = 8.5;
     public static double doubleBridgeCross = 47;
-    public static double doubleWallDistance = 9;
+    public static double doubleWallDistance = 8;
     public static double doubleCenterDistance = 9;
 
     public static double _pickUpDistance = 27;
